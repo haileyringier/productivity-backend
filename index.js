e3bf62f5e04faa7c8118fe4b53c446dd071b81c3
haileyringier <haileyringier@outlook.com>
2020-09-20 16:08:06 -0600

    attempting to delpoy with heroku

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const app = express()
-const port = '3000'
+const port = process.env.PORT || 9000
 const database = require('./database')
 const bodyParser = require('body-parser')
 const bcyrpt = require('bcrypt')
