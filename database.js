54f68b6914ee88c9d2f3a2b7b8ce9898bcf87d52
haileyringier <haileyringier@outlook.com>
2020-09-20 17:33:32 -0600

    updates database url

--- database.js
+++ database.js
@@ -1,4 +1,4 @@
-const config = require('./knexfile')[process.env.NODE_ENV || 'development'];
+const config = require('./knexfile')[process.env.DATABASE_URL || 'development'];
 const knex = require('knex');
 
 module.exports = knex(config);
