9459c47d4b1176313d5134dec2366a2608b6eeb9
haileyringier <haileyringier@outlook.com>
2020-09-20 18:01:10 -0600

    updates db url

--- knexfile.js
+++ knexfile.js
@@ -6,21 +6,10 @@ module.exports = {
     client: 'pg',
     connection: "postgres:///productivity-journal"
   },
-
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      tableName: 'knex_migrations'
+    }
 };
